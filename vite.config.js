fc3cd648c61eac17365158f249f3d1c9731b1671
alanjgo <alan.jego35@gmail.com>
2025-08-17 10:23:49 +0200

    fix main.jsx 404

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rest-countries-api-with-color-theme-switcher/',
+  // base: '/rest-countries-api-with-color-theme-switcher/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
