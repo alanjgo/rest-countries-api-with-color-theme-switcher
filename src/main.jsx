d5cacc99b19413d8ba644b570d04e9b8f6c83513
alanjgo <alan.jego35@gmail.com>
2025-08-20 22:31:27 +0200

    fix main.jsx import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { BrowserRouter, createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
